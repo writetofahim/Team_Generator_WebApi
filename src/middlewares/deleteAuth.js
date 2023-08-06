8e2a2a1ad503928eec7a601a5b99c833a990ee0d
writetofahim <writetofahimahammad@gmail.com>
2023-08-06 15:37:59 +0600

    reviewed delete player. all requirements matched

--- src/middlewares/deleteAuth.js
+++ src/middlewares/deleteAuth.js
@@ -2,13 +2,18 @@ const deleteAuth = (req, res, next) => {
   const { authorization } = req.headers;
   const token = authorization?.split(" ")[1];
 
+  if (!token) {
+    return res.status(401).json({
+      message: "Missing token",
+    });
+  }
+
   if (token === process.env.BEARER_TOKEN) {
     next();
   } else {
-    //   res.status(401).json({
-    //     message: "unauthorized access",
-    //   });
-    next("authorization fail!");
+    return res.status(401).json({
+      message: "Invalid token",
+    });
   }
 };
 
